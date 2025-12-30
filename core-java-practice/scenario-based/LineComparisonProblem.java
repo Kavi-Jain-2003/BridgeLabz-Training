281f6d811d0551ff8eb55adab6d1f0c50f76bb23
Kavi-Jain-2003 <kavijain220803@gmail.com>
2025-12-30 15:14:32 +0530

    [KaviJain] Adding all three usecases of line_comparison

--- core-java-practice/scenario-based/LineComparisonProblem.java
+++ core-java-practice/scenario-based/LineComparisonProblem.java
@@ -8,6 +8,7 @@ public class LineComparisonProblem {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Welcome to Line Comparison Computation Program");
 		System.out.println("two lines comparison:");
+		// line1
 		System.out.println("first line");
 		System.out.println("Enter coordinates for first point:");
 		int x1 = input.nextInt();
@@ -15,6 +16,7 @@ public class LineComparisonProblem {
 		System.out.println("Enter coordinates for second point:");
 		int x2 = input.nextInt();
 		int y2 = input.nextInt();
+		// line2
 		System.out.println("second line:");
 		System.out.println("Enter coordinates for first point:");
 		int x3 = input.nextInt();
@@ -26,25 +28,37 @@ public class LineComparisonProblem {
 		double lengthOfSecondLine = length(x3, y3, x4, y4);
 		System.out.println("Length Of first line is:" + lengthOfFirstLine);
 		System.out.println("Length Of second line is:" + lengthOfSecondLine);
-		String isEqual = isBothLineEquals(lengthOfFirstLine, lengthOfSecondLine);
-		System.out.println(isEqual);
+		// usecase2
+		boolean isEqual = isBothLinesEqual(lengthOfFirstLine, lengthOfSecondLine);
+		if (isEqual) {
+			System.out.println("Both lines are equal");
+		} else {
+			System.out.println("Both lines are not equal");
+		}
+		// usecase3
+		String greaterOrLesser = isGreaterOrLesser(lengthOfFirstLine, lengthOfSecondLine);
+		System.out.println(greaterOrLesser);
 		// closing input
 		input.close();
 	}
 
-	// use case2
+	// use case1
 	public static double length(int x1, int y1, int x2, int y2) {
 		double length = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
 		return length;
 	}
 
-	public static String isBothLineEquals(double lengthOfFirstLine, double lengthOfSecondLine) {
+	// use case2
+	public static boolean isBothLinesEqual(double lengthOfFirstLine, double lengthOfSecondLine) {
+		return Double.compare(lengthOfFirstLine, lengthOfSecondLine) == 0;
+	}
+
+	// use case3
+	public static String isGreaterOrLesser(double lengthOfFirstLine, double lengthOfSecondLine) {
 		if (lengthOfFirstLine < lengthOfSecondLine) {
 			return "Line2 is greater than line1";
-		} else if (lengthOfFirstLine > lengthOfSecondLine) {
-			return "Line 1 is greater than line 2";
 		}
-		return "both are equal";
+		return "Line 1 is greater than line 2";
 	}
 
 }
