127468409239a2c5824a81d591da0dbdef83139d
Kavi-Jain-2003 <kavijain220803@gmail.com>
2026-01-03 23:10:03 +0530

    [KaviJain] review-1

---
+++ VowelsAndConsonants.java
@@ -0,0 +1,36 @@
+//count vowels and consonants
+//importing scanner class
+import java.util.Scanner;
+public class VowelsAndConsonant {
+    public static void main(String[] args) {
+        //creating scanner object
+        Scanner sc=new Scanner(System.in);
+        String input=sc.nextLine();
+        countVowelAndConsonant(input);
+        //closing input
+        sc.close();
+    }
+    public static void countVowelAndConsonant(String input)
+    {
+        int len=input.length();
+        int vowels=0;
+        int consonants=0;
+        for(int i=0; i<len; i++)
+        {
+            char ch=input.charAt(i);
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+            {
+                vowels++;
+            }
+            else{
+            consonants++;
+            }
+        }
+        System.out.println("Vowels count:"+vowels);
+        System.out.println("Consonants count:"+consonants);
+
+    }
+}
+
+
+
